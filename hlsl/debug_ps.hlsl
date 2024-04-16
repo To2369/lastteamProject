3a7ff36b158c8972f00aa48dacb8d65f0b31722a
To2369 <touma06260802@gmail.com>
2024-04-16 10:28:46 +0900

    アップデート

--- hlsl/debug_ps.hlsl
+++
@@ -1,6 +0,0 @@
-#include "debug.hlsli"
-
-float4 main(VS_OUT pin) : SV_TARGET
-{
-    return pin.color;
-}
--- sources/Mathf.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-//•‚“®¬”“_Zp
-class Mathf
-{
-public:
-    //üŒ`•âŠ®
-    static float Leap(float a, float b, float t);
-};
