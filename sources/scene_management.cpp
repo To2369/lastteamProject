1253d9850d2450a9d322074e60e68d181930eec6
ggclp0129 <ggrks.999g3@gmail.com>
2024-05-22 12:48:26 +0900

    audio 改善
    中身キレイキレイ

--- sources/scene_management.cpp
+++ sources/scene_management.cpp
@@ -43,41 +43,20 @@ void SceneManagement::initialize()
 
 	//BGMê›íË
 	{
-		bgm[static_cast<int>(SCENE_BGM::SCENE_CLEAR_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\clear\\clear_jingle1.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_CLEAR_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\clear\\clear_jingle2.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_CLEAR_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\clear\\clear_jingle3.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_GAME_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\game_now\\game_now0.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_GAME_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\game_now\\game_now1.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_GAME_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\game_now\\game_now2.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_GAME_3)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\game_now\\game_now3.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_TITLE_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\title\\title_bgm0.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_TITLE_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\title\\title_bgm1.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_TITLE_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\title\\title_bgm2.wav");
-		bgm[static_cast<int>(SCENE_BGM::SCENE_TITLE_3)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\title\\title_bgm3.wav");
+		bgm[static_cast<int>(SCENE_BGM::SCENE_CLEAR)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\clear\\clear_jingle1.wav");
+		bgm[static_cast<int>(SCENE_BGM::SCENE_GAME_NOW)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\game_now\\game_now1.wav");
+		bgm[static_cast<int>(SCENE_BGM::SCENE_TITLE)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\BGM\\title\\title_bgm0.wav");
 	}
 
 	//SEê›íË
 	{
-		se[static_cast<int>(SCENE_SE::SE_BUTTON_DECISION_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\button\\button_decision_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_BUTTON_DECISION_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\button\\button_decision_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_BUTTON_DECISION_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\button\\button_decision_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_EXTRACTION_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\extraction_0\\extraction_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_EXTRACTION_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\extraction_0\\extraction_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_EXTRACTION_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\extraction_0\\extraction_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_INJECTION_SOUND_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\injection_sound\\injection_sound_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_INJECTION_SOUND_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\injection_sound\\injection_sound_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_INJECTION_SOUND_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\injection_sound\\injection_sound_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_LIFT_SOUND_EFFECTS_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\lift_sound_effects\\lift_sound_effects_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_LIFT_SOUND_EFFECTS_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\lift_sound_effects\\lift_sound_effects_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_LIFT_SOUND_EFFECTS_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\lift_sound_effects\\lift_sound_effects_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_OBJECT_MOVE_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\object_move\\object_move_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_OBJECT_MOVE_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\object_move\\object_move_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_OBJECT_MOVE_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\object_move\\object_move_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_SWITCH_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\switch\\switch_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_SWITCH_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\switch\\switch_1.wav");
-		se[static_cast<int>(SCENE_SE::SE_SWITCH_2)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\switch\\switch_2.wav");
-		se[static_cast<int>(SCENE_SE::SE_SYRINGE_INTERCALATE_0)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\syringe_intercalate\\syringe_intercalate_0.wav");
-		se[static_cast<int>(SCENE_SE::SE_SYRINGE_INTERCALATE_1)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\syringe_intercalate\\syringe_intercalate_1.wav");
+		se[static_cast<int>(SCENE_SE::SE_BUTTON_DECISION)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\button\\button_decision_0.wav");
+		se[static_cast<int>(SCENE_SE::SE_EXTRACTION)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\extraction_0\\extraction_2.wav");
+		se[static_cast<int>(SCENE_SE::SE_INJECTION_SOUND)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\injection_sound\\injection_sound_2.wav");
+		se[static_cast<int>(SCENE_SE::SE_LIFT_SOUND_EFFECTS)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\lift_sound_effects\\lift_sound_effects_2.wav");
+		se[static_cast<int>(SCENE_SE::SE_OBJECT_MOVE)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\object_move\\object_move_1.wav");
+		se[static_cast<int>(SCENE_SE::SE_SWITCH)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\switch\\switch_0.wav");
+		se[static_cast<int>(SCENE_SE::SE_SYRINGE_INTERCALATE)] = std::make_unique<audio>(xaudio2.Get(), L".\\resources\\audio\\SE\\syringe_intercalate\\syringe_intercalate_1.wav");
 	}
 }
 
